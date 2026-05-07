2994f8c8c2b382ee734dd8cc3f3066ed77945114
teamgerama <teamgerama@gmail.com>
2026-05-07 21:55:25 +0000

    Admin: Emergency database reset for migration fix

--- build.sh
+++ build.sh
@@ -5,8 +5,11 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
-python manage.py collectstatic --noinput
 
-# Run migrations to the Postgres DB
+# ... existing build steps (pip install, etc.) ...
+
+# TEMPORARY: Clear the bad data and force the migration
+python manage.py shell -c "from academy.models import Material; Material.objects.all().delete()"
+python manage.py migrate academy 0003 --fake
 python manage.py migrate
+python manage.py collectstatic --no-input
