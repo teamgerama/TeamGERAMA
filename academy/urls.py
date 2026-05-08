a22571a7c61e8eb7911adecd90565e885600803e
teamgerama <teamgerama@gmail.com>
2026-05-08 09:54:29 +0000

    Fix: Synchronized URL names between template and urls.py

--- academy/urls.py
+++ academy/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # This path must exist for the 'Explore Resources' button to work
+    path('resources/', views.home, name='resources'),
     path('programme/<int:pk>/', views.programme_detail, name='programme_detail'),
 ]
