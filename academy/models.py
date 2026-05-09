b3d0709b10999e737f0661a57df471dd8fef68e4
teamgerama <teamgerama@gmail.com>
2026-05-09 22:39:36 +0000

    Master Clean: Synced models and templates with proper related_name

--- academy/models.py
+++ academy/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# REMOVED: from .models import ... (Never import a file into itself)
-
 class School(models.Model):
     name = models.CharField(max_length=255)
     def __str__(self): return self.name
@@ -17,16 +15,8 @@ class Programme(models.Model):
     def __str__(self): return self.name
 
 class Course(models.Model):
-    LEVEL_CHOICES = [
-        ('100', 'Level 100'),
-        ('200', 'Level 200'),
-        ('300', 'Level 300'),
-        ('400', 'Level 400'),
-    ]
-    SEMESTER_CHOICES = [
-        ('1', 'Semester 1'),
-        ('2', 'Semester 2'),
-    ]
+    LEVEL_CHOICES = [('100', 'Level 100'), ('200', 'Level 200'), ('300', 'Level 300'), ('400', 'Level 400')]
+    SEMESTER_CHOICES = [('1', 'Semester 1'), ('2', 'Semester 2')]
 
     programme = models.ForeignKey(Programme, on_delete=models.CASCADE, related_name='courses')
     name = models.CharField(max_length=255)
@@ -34,12 +24,10 @@ class Course(models.Model):
     level = models.CharField(max_length=3, choices=LEVEL_CHOICES, default='100')
     semester = models.CharField(max_length=1, choices=SEMESTER_CHOICES, default='1')
 
-    def __str__(self):
-        return f"{self.code} - {self.name}"
+    def __str__(self): return f"{self.code} - {self.name}"
 
-# MOVED: Material is now back at the left margin (No longer inside Course)
 class Material(models.Model):
-    # Adding related_name='materials' makes it easier for the template to find the files
+    # 'materials' is the key word the template must use
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='materials')
     title = models.CharField(max_length=200, blank=True)
     file = models.FileField(upload_to='study_materials/')
