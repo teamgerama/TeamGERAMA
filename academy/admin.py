7411556881d4500658d817ff322869eae61a39f4
teamgerama <teamgerama@gmail.com>
2026-05-09 18:28:29 +0000

    Admin: Minimalist UI for Course and Material management

--- academy/admin.py
+++ academy/admin.py
@@ -1,20 +1,18 @@
 from django.contrib import admin
 from .models import School, Department, Programme, Course, Material
 
-
-# --- INLINES (The "Under the Parent" sections) ---
+# --- INLINES ---
+# These allow you to jump from School -> Dept -> Programme -> Course in one flow.
 
 class DepartmentInline(admin.TabularInline):
     model = Department
     extra = 1
-    show_change_link = True  # Allows you to click a link to go straight to the Dept editor
-
+    show_change_link = True
 
 class ProgrammeInline(admin.TabularInline):
     model = Programme
     extra = 1
     show_change_link = True
-
 
 class CourseInline(admin.TabularInline):
     model = Course
@@ -22,68 +20,51 @@ class CourseInline(admin.TabularInline):
     fields = ('code', 'name', 'level', 'semester')
     show_change_link = True
 
-
 class MaterialInline(admin.TabularInline):
     model = Material
     extra = 1
-
 
 # --- MAIN ADMIN INTERFACE ---
 
 @admin.register(School)
 class SchoolAdmin(admin.ModelAdmin):
-    """
-    This is your primary entry point.
-    Adding a School allows you to add Departments immediately.
-    """
     list_display = ('name',)
     search_fields = ('name',)
-    inlines = [DepartmentInline]
 
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
-    """
-    Editing a Department allows you to add Programmes immediately.
-    """
     list_display = ('name', 'school')
     list_filter = ('school',)
     search_fields = ('name',)
-    inlines = [ProgrammeInline]
 
 
 @admin.register(Programme)
 class ProgrammeAdmin(admin.ModelAdmin):
-    """
-    Editing a Programme allows you to add Courses immediately.
-    """
     list_display = ('name', 'department', 'get_school')
     list_filter = ('department__school', 'department')
     search_fields = ('name',)
-    inlines = [CourseInline]
+
 
     def get_school(self, obj):
         return obj.department.school
-
     get_school.short_description = 'School'
-
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    """
-    Editing a Course allows you to add PDF Materials immediately.
-    """
-    list_display = ('code', 'name', 'level', 'semester', 'programme')
-    list_editable = ('level', 'semester')  # Quick edits from the list view
+    # Simplified list for a cleaner "attractive" look
+    list_display = ('code', 'name', 'level', 'semester')
+    list_editable = ('level', 'semester')
     list_filter = ('programme__department__school', 'programme', 'level', 'semester')
     search_fields = ('name', 'code')
-    inlines = [MaterialInline]
 
 
 @admin.register(Material)
 class MaterialAdmin(admin.ModelAdmin):
-    list_display = ('title', 'course', 'get_level', 'file')
+    # Removed Title and redundant Dept info for a minimal look
+    list_display = ('course', 'get_level', 'file')
     list_filter = ('course__level', 'course__semester')
 
     def get_level(self, obj):
         return obj.course.level
+    get_level.short_description = 'Level'
